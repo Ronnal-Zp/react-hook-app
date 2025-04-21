ae778cfbf4605d7f15d5963b43fba1c4b1ee1a41
Aldahir Zamora <azamora@viamatica.com>
2025-04-21 15:24:23 -0500

    hook useCounter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { HooksApp } from "./HooksApp";
+// import { HooksApp } from "./HooksApp";
+import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HooksApp/>
+    {/* <HooksApp/> */}
+    <CounterWithCustomHook/>
   </StrictMode>
 )
