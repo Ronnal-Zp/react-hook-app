41fdfb0541ee6b0edd279a30e630f9515817a87a
Aldahir Zamora <ronn.zp.3@gmail.com>
2025-05-04 19:52:54 -0500

    useLayoutEffect

--- src/03-examples/PokemonCard.jsx
+++ src/03-examples/PokemonCard.jsx
@@ -1,8 +1,19 @@
+import { useLayoutEffect, useRef, useState } from "react"
+
 export const PokemonCard = ({id, name, sprites = []}) => {
+
+  const h2Ref = useRef();
+  const [boxSize, setBoxSize] = useState({ width: 0, height:  0 });
+
+  useLayoutEffect(() => {
+    const { width, height } = h2Ref.current.getBoundingClientRect();
+    setBoxSize({ width, height });
+  }, [name]);
+
   return (
     <>
       <section style={{height: 200}}>
-        <h2 className="text-capitalize">#{id} - {name}</h2>
+        <h2 ref={h2Ref} className="text-capitalize">#{id} - {name}</h2>
 
         <div>
           {
@@ -11,6 +22,8 @@ export const PokemonCard = ({id, name, sprites = []}) => {
             ))
           }
         </div>
+
+        <pre>{ JSON.stringify(boxSize) }</pre>
       </section>
     </>
   )
